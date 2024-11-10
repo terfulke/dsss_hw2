56fbb25d6ad7d595d4f72e4166026e61faaff9af
fuleova18 <135819413+fuleova18@users.noreply.github.com>
2024-11-10 22:07:37 +0100

    Clean up code setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="math_quiz",
     version="0.1",
-    packages=find_packages(),
-    install_requires=[random],
     entry_points={"console_scripts": ["math_quiz=math_quiz.math_quiz:main"]},  #main function in math_quiz.py
     description="A math quiz application",
     author="Terezia Fuleova",
